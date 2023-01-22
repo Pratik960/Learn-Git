32b758fdf48039be7e999a852c06c671beeff5b9
Pratik Solanki <ps6535754@gmail.com>
2023-01-22 18:42:50 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+class Sum{
+    public static void main(String args[]) {
+        double a = 10.5;
+        double b = 15.5;
+        
+        double sum = a + b;
+        System.out.println(sum);
+    }
+}
