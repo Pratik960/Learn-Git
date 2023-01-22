be62c072379453293914581a0d8f5e1acc4c19e6
Pratik Solanki <ps6535754@gmail.com>
2023-01-22 19:00:32 +0530

    Added Multiplication

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+class Multiply{
+    public static void main(String args[]) {
+        double a = 10.5;
+        double b = 15.5;
+        
+        double product = a * b;
+        System.out.println("The Product is :- " + product);
+    }
+}
